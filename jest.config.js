86276de70595bb9c3a309cc1ff057a4e51a3c9ee
votantai <taivo@selly.vn>
2022-09-24 08:28:44 +0700

    test commit

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ const { compilerOptions } = require("./tsconfig");
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: "./",
+  // dir: "./",
 });
 
 // Add any custom config to be passed to Jest
